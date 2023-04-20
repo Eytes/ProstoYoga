874dc64071dff393616be6c2033721e5fb1517d6
Eytes <yura.shamanov2000@bk.ru>
2023-04-20 22:17:47 +0200

    A function has been made that adds a new client

--- database/interfaces.py
+++ database/interfaces.py
@@ -1,5 +1,19 @@
-def create_client():
-    ...
+from sqlalchemy.orm import Session
+
+from database import engine
+from database.models import Client
+
+
+def create_client(name: str, lastname: str, phone: str, patronymic: str | None = None):
+    with Session(autoflush=False, bind=engine) as db:
+        if patronymic:
+            new_client = Client(name=name, lastname=lastname, patronymic=patronymic, phone=phone)
+        else:
+            new_client = Client(name=name, lastname=lastname, phone=phone)
+        db.add(new_client)
+        db.commit()
+        db.refresh(new_client)
+        return new_client.id
 
 
 def last_client_visit():
