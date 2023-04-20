01454a830ed7b6f8fc6d54cc85f02b9862220624
Eytes <yura.shamanov2000@bk.ru>
2023-04-20 21:25:56 +0200

    creating interface signatures for working with the database

--- database/__init__.py
+++ database/__init__.py
@@ -1,2 +1,4 @@
 from database.models import create_models
 from database.setup import setup
+from database import interfaces
+
---
+++ database/interfaces.py
@@ -0,0 +1,42 @@
+def create_client():
+    ...
+
+
+def last_client_visit():
+    ...
+
+
+def client_visits():
+    ...
+
+
+def subscription_purchase_day():
+    ...
+
+
+def remaining_training():
+    ...
+
+
+def subscription_end():
+    ...
+
+
+def buy_subscription():
+    ...
+
+
+def close_subscription():
+    ...
+
+
+def update_subscription():
+    ...
+
+
+def mark_visit():
+    ...
+
+
+def cancel_visit():
+    ...
