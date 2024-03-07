f3dfbbc8de35e53717ebf2e7832971d0dd4fe4ff
Eytes <yura.shamanov2000@bk.ru>
2024-03-07 18:42:39 +0300

    выделение логики работы с расписанием

--- prostoyoga/backend/src/base_dao/practices_dao.py
+++ prostoyoga/backend/src/base_dao/practices_dao.py
@@ -1,8 +1,10 @@
 from abc import ABC, abstractmethod
-from datetime import datetime
-from typing import Iterable
 
-from .base_models import PracticeModel, UserModel
+from .base_models.practice import (
+    PracticeModel,
+    CreatePracticeModel,
+    UpdatePracticeModel,
+)
 from .base_types import EntryId
 
 
@@ -13,49 +15,16 @@ class PracticesDAO(ABC):
         pass
 
     @abstractmethod
-    async def get_in_the_period(
-        self,
-        start_at: datetime,
-        end_at: datetime,
-    ) -> Iterable[PracticeModel]:
-        # TODO: добавить фильтр по типу практик
-        """Получить практики за период"""
-        pass
-
-    @abstractmethod
-    async def create(self, new_practice: PracticeModel) -> EntryId:
-        """Внести практику в расписание"""
+    async def create(self, new_practice: CreatePracticeModel) -> EntryId:
+        """Создать новую практику. Возвращает id новой практики"""
         pass
 
     @abstractmethod
     async def delete_by_id(self, practice_id: EntryId) -> EntryId:
-        """Удалить практику из расписания"""
+        """Удалить практику. Возвращает id удаленной практики"""
         pass
 
     @abstractmethod
-    async def update(self, new_practice_data: PracticeModel) -> None:
-        """Обновление данных о практике"""
-        pass
-
-    @abstractmethod
-    async def register_the_user_by_id(
-        self,
-        user_id: EntryId,
-        practice_id: EntryId,
-    ) -> None:
-        """Записать пользователя на практику"""
-        pass
-
-    @abstractmethod
-    async def remove_the_user_by_id(
-        self,
-        user_id: EntryId,
-        practice_id: EntryId,
-    ) -> None:
-        """Снять запись пользователя с практики"""
-        pass
-
-    @abstractmethod
-    async def get_registered_users(self, practice_id: EntryId) -> Iterable[UserModel]:
-        """Получить пользователей, записавшихся на занятие"""
+    async def update(self, new_practice_data: UpdatePracticeModel) -> EntryId:
+        """Обновление данных о практике. Возвращает id обновленной практики"""
         pass
