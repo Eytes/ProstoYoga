2bcf04950a25c0b240cc7687b4b0187d26d9db1e
Eytes <yura.shamanov2000@bk.ru>
2024-03-07 18:41:23 +0300

    создание моделей для записей в расписании

---
+++ prostoyoga/backend/src/base_dao/base_models/schedule.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+from .mixins import MixinStartEndTime, MixinId
+from ..base_types import EntryId
+
+
+class _PracticeInScheduleBaseModel(MixinStartEndTime):
+    practice_id: EntryId
+    master_id: EntryId
+
+
+class PracticeInScheduleModel(_PracticeInScheduleBaseModel):
+    """
+    Модель данных записи в расписании о проведении практики
+    с мастером и указанием времени начала и конца проведения практики
+    """
+
+    id: EntryId
+
+
+class CreatePracticeInScheduleModel(MixinId, _PracticeInScheduleBaseModel):
+    """
+    Модель данных для создания записи в расписании о проведении практики
+    с мастером и указанием времени начала и конца проведения практики
+    """
+
+    pass
+
+
+class UpdatePracticeInScheduleModel(_PracticeInScheduleBaseModel):
+    """
+    Модель данных для обновления записи в расписании о проведении практики
+    с мастером и указанием времени начала и конца проведения практики
+    """
+
+    practice_id: EntryId | None = None
+    master_id: EntryId | None = None
+    start_at: datetime | None = None
+    end_at: datetime | None = None
