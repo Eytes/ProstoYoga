faef58feaf668d9c23210af5857f8bcd7070c4b7
Eytes <yura.shamanov2000@bk.ru>
2024-03-08 16:36:20 +0300

    refactoring

--- prostoyoga/backend/src/base_dao/base_models/subscription.py
+++ prostoyoga/backend/src/base_dao/base_models/subscription.py
@@ -3,26 +3,35 @@ from datetime import datetime, UTC
 from pydantic import Field, BaseModel
 
 from .mixins import MixinStartEndTime, MixinId
+from ..base_types import EntryId
 
 
-class SubscriptionModel(MixinId, MixinStartEndTime):
-    """Модель для работы с абонементом (профилем пользователя)"""
+class _SubscriptionBaseModel(BaseModel, MixinStartEndTime):
+    """Базовые поля, содержащиеся во всех моделях абонемента"""
 
     first_name: str
     second_name: str
-    phone: str = Field(min_length=11, max_length=11)
-    registration_date: datetime
+    phone: str
     remaining_visits: int
 
 
-class UpdateSubscriptionModel(BaseModel):
+class SubscriptionModel(_SubscriptionBaseModel):
+    """Модель для работы с абонементом (профилем пользователя)"""
+
+    id: EntryId
+    registration_date: datetime
+
+
+class UpdateSubscriptionModel(_SubscriptionBaseModel):
     """Модель для обновления данных абонемента (профиля пользователя)"""
 
     # TODO: добавить валидацию:
     #   - имя и фамилия должны быть длиннее 2 символов и содержать только буквы
-    #   - телефон может быть только из 11 символов, если начинается с 8... или из 12, если с +7... (Сделать проверку цифр после 8/+7?)
+    #   - телефон может быть только из 11 символов, если начинается с 8... или из 12, если с +7...
+    #   (Сделать проверку цифр после 8/+7?)
     #   - дата начала занятий должна быть не меньше сегодняшней
     #   - кол-во оставшихся занятий должно быть не меньше 0
+
     first_name: str | None = None
     second_name: str | None = None
     phone: str | None = None
@@ -31,16 +40,18 @@ class UpdateSubscriptionModel(BaseModel):
     end_at: datetime | None
 
 
-class CreateSubscriptionModel(MixinId, MixinStartEndTime):
+class CreateSubscriptionModel(MixinId, _SubscriptionBaseModel):
     """Модель для создания абонемента пользователя"""
 
     # TODO: добавить валидацию:
     #   - имя и фамилия должны быть длиннее 2 символов и содержать только буквы
-    #   - телефон может быть только из 11 символов, если начинается с 8... или из 12, если с +7... (Сделать проверку цифр после 8/+7?)
+    #   - телефон может быть только из 11 символов, если начинается с 8... или из 12, если с +7...
+    #   (Сделать проверку цифр после 8/+7?)
     #   - дата должна быть не меньше сегодняшней
     #   - кол-во оставшихся занятий должно быть не меньше 0
-    first_name: str
-    second_name: str
+
     phone: str = Field(min_length=11, max_length=12)
-    registration_date: datetime = Field(default_factory=lambda: datetime.now(UTC))
+    registration_date: datetime = Field(
+        default_factory=lambda: datetime.now(UTC).date()
+    )
     remaining_visits: int = Field(ge=0, default=0)
