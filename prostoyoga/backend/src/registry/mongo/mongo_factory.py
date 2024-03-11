95cbc01825c6e8105c25d48be85b5fcab87329ec
Eytes <yura.shamanov2000@bk.ru>
2024-03-11 20:19:37 +0300

    создание конфига для registry

--- prostoyoga/backend/src/registry/mongo/mongo_factory.py
+++ prostoyoga/backend/src/registry/mongo/mongo_factory.py
@@ -1,25 +1,19 @@
-import os
-
 from motor.core import AgnosticClient, AgnosticCollection, AgnosticDatabase
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from .mongo_registry import MongoRegistry
+from ..registry_config import registry_settings
 
 
 class MongoRegistryFactory:
     def __init__(self, mongo_client: AgnosticClient | None = None) -> None:
-        if mongo_client is not None:
-            self.__mongo_client: AgnosticClient = mongo_client
-        else:
-            username = os.getenv("MONGO_USERNAME")
-            password = os.getenv("MONGO_PASSWORD")
-            port = os.getenv("PORT")
-            host = os.getenv("HOST")
-            self.__mongo_client = AsyncIOMotorClient(
-                f"mongodb://{username}:{password}@{host}:{port}"
-            )
-        db_name = os.getenv("DB_NAME")
-        self.__database: AgnosticDatabase = self.__mongo_client[db_name]
+        if not mongo_client:
+            mongo_client = AsyncIOMotorClient(registry_settings.mongo.default_url)
+
+        self.__mongo_client: AgnosticClient = mongo_client
+        self.__database: AgnosticDatabase = self.__mongo_client[
+            registry_settings.mongo.db_name
+        ]
 
     def get(self, collection_name: str) -> MongoRegistry:
         collection: AgnosticCollection = self.__database.get_collection(collection_name)
